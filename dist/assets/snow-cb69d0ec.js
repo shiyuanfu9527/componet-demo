400770382d8b10b740b5928540c0ac80eb30d50b
[9527shiyuanfu] <[985074646@163.com]>
2023-05-25 15:47:56 +0800

    add js

---
+++ dist/assets/snow-cb69d0ec.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as s,F as o,v as r,b as a}from"./index-b0c83be0.js";const c={},_={class:"snowWrapper"};function l(d,i){return e(),s("div",_,[(e(),s(o,null,r(10,n=>a("div",{class:"snow",key:n})),64))])}const u=t(c,[["render",l]]);export{u as default};
