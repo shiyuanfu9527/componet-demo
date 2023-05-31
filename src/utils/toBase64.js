5b39c5e23506dc8bb5df8ba334d1fead7d897f1b
[9527shiyuanfu] <[985074646@163.com]>
2023-05-31 10:14:28 +0800

    add

---
+++ src/utils/toBase64.js
@@ -0,0 +1,17 @@
+export default function convertImgToBase64(url, callback, outputFormat) {
+    var canvas = document.createElement('CANVAS'),
+        ctx = canvas.getContext('2d'),
+        img = new Image;
+    img.crossOrigin = 'Anonymous';
+    img.onload = function () {
+        canvas.height = img.height;
+        canvas.width = img.width;
+        ctx.drawImage(img, 0, 0);
+        var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+        callback.call(this, dataURL);
+        canvas = null;
+    };
+    img.src = url;
+}
+// 图片资源必须资源服务器设置允许跨域
+
